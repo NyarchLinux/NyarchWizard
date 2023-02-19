d023df5eb9d34dd04b32e8e9a9a62329383db4b3
SilverOS <francescocaracciolo78@gmail.com>
2023-02-19 23:44:45 +0100

    App completed

--- src/pages.py
+++ src/pages.py
@@ -1,35 +1,10 @@
 PAGES = [
 	{
-		"icon": "gaming-symbolic",
-		"title": "Gaming",
+		"icon": "nyarchwitch",
+		"title": "Welcome to Nyarch Linux Wizard",
 		"icon-size": 200,
-		"body": "The best linux softwares for gaming",
-		"apps": [
-			{
-				"title": "Steam",
-				"subtitle": "Game distribution service",
-				"icon": "steam",
-				"command": "gnome-software --details steam"
-			},
-			{
-				"title": "Bottles",
-				"subtitle": "Run windows software on Linux",
-				"icon": "bottles",
-				"command": "gnome-software --details bottles"
-			},
-			{
-				"title": "Lutris",
-				"subtitle": "Lutris helps you install and play video games",
-				"icon": "lutris",
-				"command": "gnome-software --details lutris"
-			},
-			{
-				"title": "Heroic Games Launcher",
-				"subtitle": "Heroic Games Launcher is a client to play games on Epic Games and GOG",
-				"icon": "heroic",
-				"command": "gnome-software --details com.heroicgameslauncher.hgl"
-			},
-		]
+		"body": "I'm here to show you some useful and interesting Linux apps",
+		"apps": [],
 	},
 	{
 		"icon": "office-symbolic",
@@ -66,6 +41,304 @@ PAGES = [
 				"subtitle": "Merge or split pdf documents and rotate, crop and rearrange their pages",
 				"icon": "pdfarranger",
 				"command": "gnome-software --details com.github.jeromerobert.pdfarranger"
+			},
+		]
+	},
+	{
+		"icon": "gaming-symbolic",
+		"title": "Gaming",
+		"icon-size": 200,
+		"body": "The best linux softwares for gaming",
+		"apps": [
+			{
+				"title": "Steam",
+				"subtitle": "Game distribution service",
+				"icon": "steam",
+				"command": "gnome-software --details steam"
+			},
+			{
+				"title": "Bottles",
+				"subtitle": "Run windows software on Linux",
+				"icon": "bottles",
+				"command": "gnome-software --details bottles"
+			},
+			{
+				"title": "Lutris",
+				"subtitle": "Lutris helps you install and play video games",
+				"icon": "lutris",
+				"command": "gnome-software --details lutris"
+			},
+			{
+				"title": "Heroic Games Launcher",
+				"subtitle": "Heroic Games Launcher is a client to play games on Epic Games and GOG",
+				"icon": "heroic",
+				"command": "gnome-software --details com.heroicgameslauncher.hgl"
+			},
+		]
+	},
+	{
+		"icon": "comunication-symbolic",
+		"title": "Comunication",
+		"icon-size": 200,
+		"body": "Apps to comunicate and socialize",
+		"apps": [
+			{
+				"title": "Discord",
+				"subtitle": "Discord is a free all in one messaging, voice, and video client. ",
+				"icon": "discord",
+				"command": "gnome-software --details com.discordapp.Discord"
+			},
+			{
+				"title": "Telegram",
+				"subtitle": "Telegram is an istant messaging application. ",
+				"icon": "telegram",
+				"command": "gnome-software --details org.telegram.desktop"
+			},
+			{
+				"title": "Headlines",
+				"subtitle": "Reddit client",
+				"icon": "headlines",
+				"command": "gnome-software --details io.gitlab.caveman250.headlines"
+			},
+			{
+				"title": "Fractal",
+				"subtitle": "Matrix messaging app for Gnome",
+				"icon": "fractal",
+				"command": "gnome-software --details org.gnome.Fractal"
+			},
+		]
+	},
+	{
+		"icon": "audio-symbolic",
+		"title": "Audio and Music",
+		"icon-size": 200,
+		"body": "Apps to work with audio files and listen to music",
+		"apps": [
+			{
+				"title": "Audacity",
+				"subtitle": "Record and edit audio files ",
+				"icon": "audacity",
+				"command": "gnome-software --details org.audacityteam.Audacity"
+			},
+			{
+				"title": "Amberol",
+				"subtitle": "Music player for Gnome. ",
+				"icon": "amberol",
+				"command": "gnome-software --details io.bassi.Amberol"
+			},
+			{
+				"title": "Mousai",
+				"subtitle": "Identify any songs in seconds.",
+				"icon": "mousai",
+				"command": "gnome-software --details io.github.seadve.Mousai"
+			},
+			{
+				"title": "Spotify",
+				"subtitle": "Official spotify client for Linux.",
+				"icon": "spotify",
+				"command": "gnome-software --details com.spotify.Client"
+			},
+			{
+				"title": "Spot",
+				"subtitle": "Unofficial Spotify Client for Gnome",
+				"icon": "spot",
+				"command": "gnome-software --details dev.alextren.Spot"
+			},
+		]
+	},
+	{
+		"icon": "photo-symbolic",
+		"title": "Image Editing",
+		"icon-size": 200,
+		"body": "Apps to edit images",
+		"apps": [
+			{
+				"title": "GIMP",
+				"subtitle": "Create images and edit photographs",
+				"icon": "gimp",
+				"command": "gnome-software --details org.gimp.GIMP"
+			},
+			{
+				"title": "Krita",
+				"subtitle": "Digital painting art studio. ",
+				"icon": "krita",
+				"command": "gnome-software --details Krita"
+			},
+			{
+				"title": "Inkscape",
+				"subtitle": "Vector graphics editor.",
+				"icon": "inkscape",
+				"command": "gnome-software --details org.inkscape.Inkscape"
+			},
+			{
+				"title": "Darktable",
+				"subtitle": "Organize and develop images from digital cameras",
+				"icon": "darktable",
+				"command": "gnome-software --details org.darktable.Darktable"
+			},
+			{
+				"title": "Image Roll",
+				"subtitle": "Image viewer with basic image manipulation tools",
+				"icon": "Imageroll",
+				"command": "gnome-software --details com.github.weclaw1.ImageRoll"
+			},
+		]
+	},
+	{
+		"icon": "video-symbolic",
+		"title": "Video Editing",
+		"icon-size": 200,
+		"body": "Apps to edit videos",
+		"apps": [
+			{
+				"title": "OBS Studio",
+				"subtitle": "Live streaming and video recording software",
+				"icon": "obs",
+				"command": "gnome-software --details com.obsproject.Studio"
+			},
+			{
+				"title": "Blender",
+				"subtitle": "Free and open source 3D creation suite",
+				"icon": "blender",
+				"command": "gnome-software --details blender"
+			},
+			{
+				"title": "Kdenlive",
+				"subtitle": "Video editor.",
+				"icon": "kdenlive",
+				"command": "gnome-software --details org.kde.kdenlive"
+			},
+			{
+				"title": "Pitivi",
+				"subtitle": "Video editor for Gnome",
+				"icon": "pitivi",
+				"command": "gnome-software --details pitivi"
+			},
+			{
+				"title": "Video Trimmer",
+				"subtitle": "Trim video quickly",
+				"icon": "videotrimmer",
+				"command": "gnome-software --details org.gnome.gitlab.YaLTeR.VideoTrimmer"
+			},
+		]
+	},
+	{
+		"icon": "developer-symbolic",
+		"title": "Developer Tools",
+		"icon-size": 200,
+		"body": "Useful tools for developers",
+		"apps": [
+			{
+				"title": "Code - OSS",
+				"subtitle": "Open Source Visual Studio Code, powerful text editor",
+				"icon": "codeoss",
+				"command": "gnome-software --details com.visualstudio.code-oss"
+			},
+			{
+				"title": "Visual Studio Code",
+				"subtitle": "Powerful text and code editor",
+				"icon": "vscode",
+				"command": "gnome-software --details com.visualstudio.code"
+			},
+			{
+				"title": "Builder",
+				"subtitle": "IDE for Gnome",
+				"icon": "builder",
+				"command": "gnome-software --details org.gnome.Builder"
+			},
+			{
+				"title": "Atom",
+				"subtitle": "A hackable text editor for the 21st Century",
+				"icon": "atom",
+				"command": "gnome-software --details io.atom.Atompitivi"
+			},
+			{
+				"title": "Meld",
+				"subtitle": "Compare and merge your files",
+				"icon": "meld",
+				"command": "gnome-software --details org.gnome.meld"
+			},
+			{
+				"title": "Github Desktop",
+				"subtitle": "Official github client for Linux",
+				"icon": "github",
+				"command": "gnome-software --details io.github.shiftey.Desktop"
+			},
+		]
+	},
+	{
+		"icon": "notes-symbolic",
+		"title": "Note Taking",
+		"icon-size": 200,
+		"body": "Apps built for note-taking",
+		"apps": [
+			{
+				"title": "Rnote",
+				"subtitle": "Sketch and take handwritten notes",
+				"icon": "rnote",
+				"command": "gnome-software --details com.github.flxzt.rnote"
+			},
+			{
+				"title": "Xournal++",
+				"subtitle": "Xournal++ is a hand note taking software.",
+				"icon": "xournalpp",
+				"command": "gnome-software --details com.github.xournalpp.xournalpp"
+			},
+			{
+				"title": "LogSeq",
+				"subtitle": "A local-first, non-linear, outliner notebook for organizing notes",
+				"icon": "logseq",
+				"command": "gnome-software --details com.logseq.Logseq"
+			},
+			{
+				"title": "Planner",
+				"subtitle": "Never worry about forgetting things again",
+				"icon": "planner",
+				"command": "gnome-software --details com.github.alainm23.planner"
+			},
+			{
+				"title": "Iotas",
+				"subtitle": "Simple note taking",
+				"icon": "iotas",
+				"command": "gnome-software --details org.gnome.gitlab.cheywood.Iotas"
+			},
+		]
+	},
+	{
+		"icon": "other-symbolic",
+		"title": "Other useful apps",
+		"icon-size": 200,
+		"body": "Some more apps you might like",
+		"apps": [
+			{
+				"title": "Atoms",
+				"subtitle": "Easily manage Linux Chroot and Containers.",
+				"icon": "atoms",
+				"command": "gnome-software --details pm.mirko.Atoms"
+			},
+			{
+				"title": "Gnome Boxes",
+				"subtitle": "Simple virtualization software.",
+				"icon": "boxes",
+				"command": "gnome-software --details org.gnome.Boxes"
+			},
+			{
+				"title": "Bitwarden",
+				"subtitle": "A secure and free password manager for all of your devices",
+				"icon": "bitwarden",
+				"command": "gnome-software --details com.bitwarden.desktop"
+			},
+			{
+				"title": "Nextcloud Desktop",
+				"subtitle": "Nextcloud desktop synchronization client",
+				"icon": "nextcloud",
+				"command": "gnome-software --details com.nextcloud.desktopclient.nextcloud"
+			},
+			{
+				"title": "Syncthing GTK",
+				"subtitle": "GUI and notification area icon for Syncthing, synchronization software",
+				"icon": "syncthing",
+				"command": "gnome-software --details me.kozec.syncthingtk"
 			},
 		]
 	},
