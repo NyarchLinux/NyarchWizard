94c3a90b85a4302b487b4dcd6cc68b7518e06880
SilverOS <francescocaracciolo78@gmail.com>
2023-02-19 22:19:23 +0100

    Office and Gaming sections done

--- src/pages.py
+++ src/pages.py
@@ -23,31 +23,49 @@ PAGES = [
 				"icon": "lutris",
 				"command": "gnome-software --details lutris"
 			},
+			{
+				"title": "Heroic Games Launcher",
+				"subtitle": "Heroic Games Launcher is a client to play games on Epic Games and GOG",
+				"icon": "heroic",
+				"command": "gnome-software --details com.heroicgameslauncher.hgl"
+			},
 		]
 	},
 	{
-		"icon": "gaming-symbolic",
+		"icon": "office-symbolic",
 		"title": "Office",
 		"icon-size": 200,
 		"body": "Choose these programs to work with office files",
 		"apps": [
 			{
-				"title": "Steam",
-				"subtitle": "Game distribution service",
-				"icon": "steam",
-				"command": "gnome-software --details steam"
+				"title": "LibreOffice",
+				"subtitle": "LibreOffice is a powerful office suite. ",
+				"icon": "libreoffice",
+				"command": "gnome-software --details org.libreoffice.LibreOffice"
 			},
 			{
-				"title": "Bottles",
-				"subtitle": "Run windows software on Linux",
-				"icon": "bottles",
-				"command": "gnome-software --details bottles"
+				"title": "ONLYOFFICE Desktop Editors",
+				"subtitle": "Create, view and edit text documents, spreadsheets and presentations. ",
+				"icon": "onlyoffice",
+				"command": "gnome-software --details org.onlyoffice.desktopeditors"
 			},
 			{
-				"title": "Lutris",
-				"subtitle": "Lutris helps you install and play video games",
-				"icon": "lutris",
-				"command": "gnome-software --details lutris"
+				"title": "Xournal++",
+				"subtitle": "Xournal++ is a hand note taking software.",
+				"icon": "xournalpp",
+				"command": "gnome-software --details com.github.xournalpp.xournalpp"
+			},
+			{
+				"title": "TexMatch",
+				"subtitle": "TeX Match allows you to search through over 1000 different LaTeX symbols by sketching.",
+				"icon": "texmatch",
+				"command": "gnome-software --details fyi.zoey.TeX-Match"
+			},
+			{
+				"title": "PDF Arranger",
+				"subtitle": "Merge or split pdf documents and rotate, crop and rearrange their pages",
+				"icon": "pdfarranger",
+				"command": "gnome-software --details com.github.jeromerobert.pdfarranger"
 			},
 		]
 	},
